b087faddf96dea19a336eb35ac819ab8cfa03ab7
saisowhit <38743607+saisowhit@users.noreply.github.com>
2023-09-21 12:50:27 -0400

    Update configuration.py

--- custom_connector_example/handlers/configuration.py
+++ custom_connector_example/handlers/configuration.py
@@ -18,7 +18,8 @@ API_VERSION = 'v51.0'
 API_VERSION_KEY = 'api_version'
 IS_SANDBOX_ACCOUNT = "is_sandbox_account"
 
-AUTH_URL = 'https://login.microsoftonline.com/6197edc2-01c0-4b24-8919-8f827d5c4dfa/oauth2/token'
+
+AUTH_URL = 'https://login.microsoftonline.com/common/oauth2/authorize?resource=https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com'
 TOKEN_URL = 'https://login.microsoftonline.com/6197edc2-01c0-4b24-8919-8f827d5c4dfa/oauth2/token'
 SANDBOX_AUTH_URL = 'https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com/data/RetailStores/'
 SANDBOX_TOKEN_URL = 'https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com/data/RetailStores'
