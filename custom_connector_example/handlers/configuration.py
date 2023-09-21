d771f675e3a46b9fc39abcb587ca1978688c5512
saisowhit <38743607+saisowhit@users.noreply.github.com>
2023-09-21 18:01:14 -0400

    Update configuration.py

--- custom_connector_example/handlers/configuration.py
+++ custom_connector_example/handlers/configuration.py
@@ -20,16 +20,14 @@ IS_SANDBOX_ACCOUNT = "is_sandbox_account"
 
 
 # AUTH_URL = 'https://login.microsoftonline.com/common/oauth2/authorize?resource=https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com'
-AUTH_URL = 'https://login.microsoftonline.com/common/oauth2/authorize?resource=https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com? CLIENT_ID='66827430-a267-471d-8a79-06940b2792a4'
-& GRANT_TYPE='client_credentials'& CLIENT_SECRET='Zn18Q~VwrI9orkNUHtbpweVmJcsZJpxK1~2Ebdfn'
-TOKEN_URL = 'https://login.microsoftonline.com/common/oauth2/authorize?resource=https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com?CLIENT_ID='66827430-a267-471d-8a79-06940b2792a4'
-&GRANT_TYPE='client_credentials'&CLIENT_SECRET='Zn18Q~VwrI9orkNUHtbpweVmJcsZJpxK1~2Ebdfn'
-SANDBOX_AUTH_URL = 'https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com/data/data/'
+AUTH_URL = 'https://login.microsoftonline.com/common/oauth2/authorize?resource=https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com'
+TOKEN_URL ='https://login.microsoftonline.com/common/oauth2/authorize?resource=https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com'
+SANDBOX_AUTH_URL ='https://login.microsoftonline.com/common/oauth2/authorize?resource=https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com'
 SANDBOX_TOKEN_URL = 'https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com/data/data'
 REFRESH_URL = 'https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com/data/'
 REDIRECT_URL = 'https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com/data/data/'
 SALESFORCE_USERINFO_URL_FORMAT = 'https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com/data/data/'
-SALESFORCE_USERINFO_SANDBOX_URL_FORMAT = 'https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com/data/data/'
+SALESFORCE_USERINFO_SANDBOX_URL_FORMAT = 'https://amtrakdev106c5dd72b7ca8401cdevaos.axcloud.dynamics.com/data/'
 TRUE = 'true'
 
 def buildSalesforceUserInfoRequest(connector_runtime_settings: dict) -> str:
